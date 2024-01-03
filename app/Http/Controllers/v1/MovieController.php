d9de5ae223fd570b7349aa9b11fc7fc6965405df
algrvvv <alexandrgr25@gmail.com>
2024-01-03 18:14:40 +0300

    7: написаны вывод и добавление фильмов
    Создан AuthController. Написаны методы для вывода всех фильмов, фильмов по айди, а также добавление нового фильма

---
+++ app/Http/Controllers/v1/MovieController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MovieStoreRequest;
+use App\Http\Requests\MovieUpdateRequest;
+use App\Http\Resources\MovieResource;
+use App\Models\Movie;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return MovieResource::collection(Movie::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(MovieStoreRequest $request): MovieResource
+    {
+        return new MovieResource(Movie::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Movie $movie): MovieResource
+    {
+        return new MovieResource($movie);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(MovieUpdateRequest $request, Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
